5300d511120967690d74cadc9aac4ac6b9c5fb9e
Ritik13012002 <ritiksingh1312002@gmail.com>
2026-09-21 18:09:35 +0530

    add text_stats function and test

--- text_stats.py
+++ text_stats.py
@@ -10,15 +10,6 @@ text = text.lower()
 
 text = text.translate(str.maketrans("", "", string.punctuation))
 
-frequency = {}
-for word in text.split():
-    if word in frequency:
-        frequency[word] += 1
-    else:
-        frequency[word]=1
-
-print(frequency)
-
 # frequency = Counter(text.split())
 # print(frequency)
 # top10 = frequency.most_common(3)
@@ -26,5 +17,22 @@ print(frequency)
 # for word,count in top10:
 #     print(word,count)
 
-sorted_frequency = sorted(frequency.items(),key=lambda x:x[1],reverse=True)
-print(sorted_frequency[:10])
+def clean_text(text):
+    text = text.lower()
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    return text
+def load_text(file_path):
+    with open(file_path, "r",encoding = "utf-8") as file:
+        return file.read()
+
+def word_count(text):
+    text = clean_text(text)
+    return len(text.split())
+
+def unique_word_count(text):
+    text = clean_text(text)
+    return len(set(text.split()))
+
+if __name__ == "__main__":
+    text = load_text("article.txt")
+    print(f"Word count: {word_count(text)} Unique word count: {unique_word_count(text)}")
